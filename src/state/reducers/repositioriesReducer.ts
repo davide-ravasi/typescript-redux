865edf08c0631d2433e4dbc45b38eba6c6ba4edc
davide.ravasi <davide.ravasi@brandalley.com>
2021-04-08 10:41:32 +0200

    reorganized redux types and actions

--- src/state/reducers/repositioriesReducer.ts
+++ src/state/reducers/repositioriesReducer.ts
@@ -1,32 +1,10 @@
+import { Action } from "../actions";
+import { ActionTypes } from "../action-types";
+
 interface RepositoriesState {
   loading: boolean;
   error: string | null;
   repositories: string[];
-}
-
-interface SearchRepositoriesAction {
-  type: ActionTypes.SEARCH_REPOSITORIES;
-}
-
-interface SearchRepositoriesSuccessAction {
-  type: ActionTypes.SEARCH_REPOSITORIES_SUCCESS;
-  payload: string[];
-}
-
-interface SearchRepositoriesErrorAction {
-  type: ActionTypes.SEARCH_REPOSITORIES_ERROR;
-  payload: string;
-}
-
-type Action =
-  | SearchRepositoriesAction
-  | SearchRepositoriesSuccessAction
-  | SearchRepositoriesErrorAction;
-
-enum ActionTypes {
-  SEARCH_REPOSITORIES = "search_repositories",
-  SEARCH_REPOSITORIES_SUCCESS = "search_repositories_success",
-  SEARCH_REPOSITORIES_ERROR = "search_repositories_error",
 }
 
 const reducer = (
