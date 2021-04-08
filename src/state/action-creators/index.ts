db04804785806a330edd9f3d5ca9c2f1e9d62456
davide.ravasi <davide.ravasi@brandalley.com>
2021-04-08 11:10:31 +0200

    created action creator logic

---
+++ src/state/action-creators/index.ts
@@ -0,0 +1,34 @@
+import axios from "axios";
+import { ActionTypes } from "../action-types";
+import { Action } from "../actions";
+
+export const searchRepositories = (term: string) => {
+  return async (dispatch: any) => {
+    dispatch({ type: ActionTypes.SEARCH_REPOSITORIES });
+
+    try {
+      const { data } = await axios.get(
+        "http://registry.npmjs.org/-/v1/search",
+        {
+          params: {
+            text: term,
+          },
+        }
+      );
+
+      const names = data.objects.map((el: any) => {
+        return el.package.name;
+      });
+
+      dispatch({
+        type: ActionTypes.SEARCH_REPOSITORIES_SUCCESS,
+        payload: names,
+      });
+    } catch (err) {
+      dispatch({
+        type: ActionTypes.SEARCH_REPOSITORIES_ERROR,
+        payload: err.message,
+      });
+    }
+  };
+};
